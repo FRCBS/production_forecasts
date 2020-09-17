009b8c3545ddbc8a90325cece97e396b4dade2da
Esa <33955910+theSway@users.noreply.github.com>
2020-09-17 21:28:15 +0300

    Fix more critical bugs
    startdate --> date

--- src/pffunctions.R
+++ src/pffunctions.R
@@ -42,7 +42,7 @@ aggregate_weekly <- function(series){
     weeklist[[k]] <- j
     datelist[[k]] <- series$date[i]
   }
-  weekly <- data.frame(week = unlist(weeklist), startdate = as.Date.numeric(unlist(datelist), origin = "1970-01-01"), pcs = unlist(pcslist))
+  weekly <- data.frame(week = unlist(weeklist), date = as.Date.numeric(unlist(datelist), origin = "1970-01-01"), pcs = unlist(pcslist))
   return(weekly)
 }
 
@@ -659,7 +659,7 @@ chosen_forecast <- function(chosen.model, series.ts, monthly, freq = "monthly"){
       
       # NNETAR
       nn.fit <- nnetar(train)
-      nn.fcast <- forecast(nn.fit, h = 4)
+      nn.fcast <- forecast(nn.fit, h = 4)$mean
       
       forecasts <- colMeans(matrix(c(snaive.fcast, ma5.fcast, ma7.fcast, ma9.fcast, ma12.fcast, stl.fcast,
                                      ets.fcast, tbats.fcast, stlf.fcast, arimax.fcast, dynreg.fcast, nn.fcast), 
@@ -814,7 +814,7 @@ chosen_forecast_extended <- function(chosen.model, series.ts, monthly, freq = "m
       
       # NNETAR
       nn.fit <- nnetar(train)
-      nn.fcast <- forecast(nn.fit, h = 23)
+      nn.fcast <- forecast(nn.fit, h = 23)$mean
       
       forecasts <- colMeans(matrix(c(snaive.fcast, ma5.fcast, ma7.fcast, ma9.fcast, ma12.fcast, stl.fcast,
                                      ets.fcast, tbats.fcast, stlf.fcast, arimax.fcast, dynreg.fcast, nn.fcast), 
@@ -869,12 +869,12 @@ draw_forecast <- function(forecast_dataframe, freq, history, type, modelname, pa
       theme(legend.position = "bottom", legend.margin = margin(t = -20, b = 0)) + 
       scale_colour_manual(values = c(palette[["colData"]], palette[["colPred"]]), labels = c("Data", "Forecast"))
   } else {
-    ggplot(data = forecast_dataframe, aes(x = seq.Date(from = tail(history$startdate, 1), to = tail(history$startdate, 1) + weeks(3), by = "week"), y = fcast)) + 
-      geom_segment(x = tail(history$startdate, 2)[1], y = tail(history[,type], 2)[1], xend = tail(history$startdate, 1), yend = head(forecast_dataframe$fcast, 1), color = "#D55E00", linetype = "dashed", alpha = palette[["alphaSeg"]]) +
+    ggplot(data = forecast_dataframe, aes(x = seq.Date(from = tail(history$date, 1), to = tail(history$date, 1) + weeks(3), by = "week"), y = fcast)) + 
+      geom_segment(x = tail(history$date, 2)[1], y = tail(history[,type], 2)[1], xend = tail(history$date, 1), yend = head(forecast_dataframe$fcast, 1), color = "#D55E00", linetype = "dashed", alpha = palette[["alphaSeg"]]) +
       geom_ribbon(data = forecast_dataframe, aes(ymin = lower95, ymax = upper95), alpha = palette[["alpha95"]], fill = palette[["fill95"]]) + 
       geom_ribbon(data = forecast_dataframe, aes(ymin = lower80, ymax = upper80), alpha = palette[["alpha80"]], fill = palette[["fill80"]]) +
       geom_line(aes(colour = "Forecast"), size = 0.8) +
-      geom_line(data = tail(head(history, -1), 16), aes(x = startdate, y = get(type), colour = palette[["colData"]]), size = 0.8, alpha = palette[["alphaSeg"]] ) +
+      geom_line(data = tail(head(history, -1), 16), aes(x = date, y = get(type), colour = palette[["colData"]]), size = 0.8, alpha = palette[["alphaSeg"]] ) +
       
       labs(title = paste0("4 week forecast: ", type),
            subtitle = paste0("Best performing model of the previous year: ", modelname),
@@ -910,7 +910,7 @@ save_forecast <- function(fdf, months = TRUE, modelname, history, file, reverse_
                             lower80 = fdf$lower80[1] * reverse_adj[1],
                             lower95 = fdf$lower95[1] * reverse_adj[1], stringsAsFactors = F) 
   } else{
-    save.this <- data.frame(time = tail(history$startdate, 1), 
+    save.this <- data.frame(time = tail(history$date, 1), 
                             model = modelname, 
                             forecast = fdf$fcast[1],
                             upper80 = fdf$upper80[1],
@@ -972,15 +972,15 @@ save_forecast <- function(fdf, months = TRUE, modelname, history, file, reverse_
         }
       } else{
         for(mdate in missing){
-          cutreal<- history[history$startdate <= mdate, ] # Cut real history at the missing date
-          wbeginning <- head(tail(cutreal$startdate, 209), 1) # Define beginning of 4 year window 
+          cutreal<- history[history$date <= mdate, ] # Cut real history at the missing date
+          wbeginning <- head(tail(cutreal$date, 209), 1) # Define beginning of 4 year window 
           segment <- head(tail(cutreal[, type], 209), 208) # Define 4 year window
           series.ts <- ts(segment, start = decimal_date(wbeginning), frequency = 52) # Transform into a ts object
           
           # Forecast
           chosen.model <- select_model(beginning, series.ts, freq = "weekly") # Choose model
           mcast <- chosen_forecast(chosen.model, series.ts, history, freq = "weekly") # Output a forecast
-          missing.fcast <- data.frame(time = tail(cutreal$startdate, 1), 
+          missing.fcast <- data.frame(time = tail(cutreal$date, 1), 
                                       model = modelnames[chosen.model], 
                                       forecast = mcast$fcast[1],
                                       upper80 = mcast$upper80[1],
@@ -1053,14 +1053,14 @@ save_forecast <- function(fdf, months = TRUE, modelname, history, file, reverse_
       
       for(i in seq(0, 23)){
         cutreal <- head(history, -(24-i)) # Erase (24-x) weeks from real history
-        wbeginning <- head(tail(cutreal$startdate, 209), 1) # Define beginning of 4 year window 
+        wbeginning <- head(tail(cutreal$date, 209), 1) # Define beginning of 4 year window 
         segment <- head(tail(cutreal[, type], 209), 208)
         series.ts <- ts(segment, start = decimal_date(wbeginning), frequency = 52)
         
         # Forecast
         chosen.model <- select_model(wbeginning, series.ts, "weekly") # Choose model
         mcast <- chosen_forecast(chosen.model, series.ts, history, freq = "weekly") # Output a forecast
-        missing.fcast <- data.frame(time = tail(cutreal$startdate, 1), 
+        missing.fcast <- data.frame(time = tail(cutreal$date, 1), 
                                     model = modelnames[chosen.model], 
                                     forecast = mcast$fcast[1],
                                     upper80 = mcast$upper80[1],
@@ -1111,7 +1111,7 @@ draw_history <- function(forecasthistory, history, freq = "monthly", type, palet
     
     p <- ggplot(data = fcast.history, aes(x = as.Date(time), y = forecast, colour = "Forecast"), label = model) +
       geom_line(aes(colour = "Forecast"), size = 0.9, alpha = palette[["alphaSeg"]]) +
-      geom_line(data = head(history, -1), aes(x = startdate, y = get(type), colour = "Data"), size = 0.8, alpha = palette[["alphaSeg"]]) +
+      geom_line(data = head(history, -1), aes(x = date, y = get(type), colour = "Data"), size = 0.8, alpha = palette[["alphaSeg"]]) +
       labs(title = "Weekly history (6 months)",
            subtitle = paste0("Mean error: ", round(mean(fcast.history$err), 2), "%"),
            x = "",
